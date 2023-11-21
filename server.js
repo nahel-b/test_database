7bd44dfbfb49074f7132492c8825f61f95b26eb3
nahel-b <nahelbelma@gmail.com>
2023-11-21 14:06:04 +0100

    changement : sqlite -> mongodb

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'votre_cle_secrete', resave: true, saveUninitialized: true }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 app.set('view engine', 'ejs');
 
-// Database setup
-const db = new sqlite3.Database('utilisateurs.db');
+// Connecter à MongoDB Atlas
+const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+let db;
 
-db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS utilisateurs (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)');
-});
+client.connect()
+  .then(() => {
+    console.log('Connecté à MongoDB Atlas');
+    db = client.db();
+  })
+  .catch(err => console.error('Erreur de connexion à MongoDB Atlas', err));
 
 // Route pour la page d'accueil
 app.get('/', (req, res) => {
@@ -30,7 +35,7 @@ app.get('/', (req, res) => {
 
 // Route pour la page de connexion
 app.get('/login', (req, res) => {
-  res.render('login', { erreur: null }); // Assurez-vous que 'erreur' est défini
+  res.render('login', { erreur: null });
 });
 
 // Route pour gérer la connexion
@@ -41,14 +46,12 @@ app.post('/login', async (req, res) => {
   const utilisateur = await chercherUtilisateur(username);
 
   if (utilisateur && await bcrypt.compare(password, utilisateur.password)) {
-    req.session.utilisateur = { id: utilisateur.id, username: utilisateur.username };
+    req.session.utilisateur = { id: utilisateur._id, username: utilisateur.username };
     res.redirect('/');
   } else {
     res.render('login', { erreur: 'Nom d\'utilisateur ou mot de passe incorrect' });
   }
 });
-
-
 
 // Route pour la page d'inscription
 app.get('/signup', (req, res) => {
@@ -63,13 +66,9 @@ app.post('/signup', async (req, res) => {
   const hash = await bcrypt.hash(password, 10);
 
   // Enregistrez les nouvelles informations d'identification dans la base de données
-  db.run('INSERT INTO utilisateurs (username, password) VALUES (?, ?)', [username, hash], (err) => {
-    if (err) {
-      res.render('signup', { erreur: 'Erreur lors de l\'inscription' });
-    } else {
-      res.redirect('/login');
-    }
-  });
+  await db.collection('utilisateurs').insertOne({ username, password: hash });
+
+  res.redirect('/login');
 });
 
 // Route pour la déconnexion
@@ -85,14 +84,5 @@ app.listen(port, () => {
 
 // Fonction pour chercher un utilisateur dans la base de données
 function chercherUtilisateur(username) {
-  return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM utilisateurs WHERE username = ?', [username], (err, row) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(row);
-      }
-    });
-  });
+  return db.collection('utilisateurs').findOne({ username });
 }
-
